234937f76e4a643b75777e3a30506a9e8eb852f3
vishal <vishalkhandekar1026@gmail.com>
2019-08-13 15:33:45 +0530

    login

--- invetory/inventoryApp/views.py
+++ invetory/inventoryApp/views.py
@@ -1,8 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render
+from models import *
 
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    if request.method == 'POST':
+        return HttpResponse("User exist")
+    return HttpResponse("Last")
+
+
+@csrf_protect
+def login(request):
+    if request.method == 'POST':
+        try:
+            result = Inventory_users.objects.get(user_name=request.POST['userName'], password=request.POST['password'])
+            print result
+            if result:
+                request.session['user_id'] = result.user_id
+                request.session['user_name'] = result.user_name
+                request.session['user_type'] = result.user_type
+        except Inventory_users.DoesNotExist:
+            return render(request, 'login.html',{'invalidcredential':True})
+    return HttpResponse("User exist")
+
