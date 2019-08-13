ecf9edec3102531d8b4c0be1fa59abe00dcc97e1
vishal <vishalkhandekar1026@gmail.com>
2019-08-13 21:12:13 +0530

    registration done

--- invetory/inventoryApp/views.py
+++ invetory/inventoryApp/views.py
@@ -13,12 +13,17 @@ def register(request):
     if request.method == 'GET':
         return render(request, 'register.html', {'userExist':False})
     if request.method == 'POST':
-        result = Inventory_users.objects.get(user_name=request.POST['userName'])
-        # print result
-        if result:
+        try:
+            result = Inventory_users.objects.get(user_name=request.POST['userName'])
             return render(request, 'register.html', {'userExist':True})
-        return HttpResponse("User exist")
-    return HttpResponse("Last")
+        except:
+            newUser = Inventory_users()
+            newUser.first_name=request.POST['userName']
+            newUser.last_name=request.POST['userName']
+            newUser.user_name=request.POST['userName']
+            newUser.password=request.POST['password']
+            newUser.save()
+    return render(request, 'login.html',{'userRegisteredSuccessfully':True, 'userExist':False})
 
 
 @csrf_protect
